80c409cbd4c7201fa6aef7fb05aea2b56314d956
ErrorCHK <147150752+ErrorCHK@users.noreply.github.com>
2024-03-26 19:50:43 +0530

    Delete gates directory

--- gates/Stripe 5$.php
+++
@@ -1,9 +0,0 @@
-  <?php
-
-    if (preg_match('/^(\/sr|\.sr|!sr)/', $text)) {
-      $userid = $update['message']['from']['id'];
-
-        $sent_message_id = send_reply($chatId, $message_id, $keyboard, "<b>THIS GATE ON MAINTENANCE ❌</b>");
-
-
-  }
--- gates/Stripe 5.48$.php
+++
@@ -1,9 +0,0 @@
-  <?php
-
-    if (preg_match('/^(\/stp|\.stp|!stp)/', $text)) {
-      $userid = $update['message']['from']['id'];
-
-        $sent_message_id = send_reply($chatId, $message_id, $keyboard, "<b>THIS GATE ON MAINTENANCE ❌</b>");
-
-
-  }
